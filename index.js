ead79154715cbe91cf50e9c811d7b57eb0aee5a5
MindeVieras <mindevieras@gmail.com>
2022-05-28 00:21:59 +0100

    Full version from tutorial.

--- index.js
+++ index.js
@@ -1,20 +1,3 @@
-module.exports = {
-  globals: {
-    React: true,
-    JSX: true,
-  },
-  parser: '@typescript-eslint/parser',
-  plugins: ['@typescript-eslint', 'jsdoc'],
-  extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/eslint-recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:jsdoc/recommended'
-  ],
-  rules: {
-    'jsdoc/tag-lines': 0,
-  },
-  parserOptions: {
-    project: './tsconfig.json',
-  },
-};
+const eslintrc = require('./.eslintrc');
+
+module.exports = eslintrc;
