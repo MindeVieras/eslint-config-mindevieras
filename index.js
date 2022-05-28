fb1c2c31cb34c717a0f753dc474bf44d71ee3335
MindeVieras <mindevieras@gmail.com>
2022-05-28 01:44:19 +0100

    move prettier config to index.

--- index.js
+++ index.js
@@ -1,14 +1,25 @@
 module.exports = {
   parser: '@typescript-eslint/parser',
-  plugins: ['@typescript-eslint', 'jsdoc'],
+  plugins: ['@typescript-eslint', 'jsdoc', 'prettier'],
   extends: [
+    'prettier',
     'plugin:@typescript-eslint/recommended',
     'plugin:jsdoc/recommended',
-    // Layer in all the JS Rules
     './.eslintrc.js',
   ],
   rules: {
     'jsdoc/tag-lines': 0,
+    'prettier/prettier': [
+      'error',
+      {
+        trailingComma: 'es5',
+        singleQuote: true,
+        printWidth: 120,
+        // below line only for windows users facing CLRF and eslint/prettier error
+        // non windows users feel free to delete it
+        endOfLine: 'auto',
+      },
+    ],
   },
   parserOptions: {
     project: './tsconfig.json',
